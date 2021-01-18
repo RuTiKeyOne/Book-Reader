5538c17299ec46787babdddbdd4557ae29ddb91d
rutikeyone <rutikeyone@gmail.com>
2021-01-18 13:45:07 +0700

    Add commands

---
+++ BookReader/ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace BookReader.ViewModel.Base
+{
+    public class BaseViewModel
+    {
+        public RelayCommand<Window> Close { get; set; }
+        public void CloseWindow(Window window)
+        {
+            if(window != null)
+            {
+                window.Close();
+            }
+        }
+
+        public RelayCommand<Window> Minimize { get; set; }
+        public void MinimizeWindow(Window window)
+        {
+            if(window != null)
+            {
+                window.WindowState = WindowState.Minimized;
+            }
+        }
+
+        public BaseViewModel()
+        {
+            Close = new RelayCommand<Window>(this.CloseWindow);
+            Minimize = new RelayCommand<Window>(this.MinimizeWindow);
+        }
+    }
+}
---
+++ BookReaderLibrary/Model/Commands/ActionCommand.cs
@@ -0,0 +1,21 @@
+﻿using BookReaderLibrary.Model.Commands.Base;
+using System;
+
+namespace BookReaderLibrary.Model.Commands
+{
+    public class ActionCommand : BaseCommand
+    {
+        private Action<object> _Execute { get; set; }
+        private Func<object, bool> _CanExecute { get; set; }
+
+        public ActionCommand(Action<object> execute, Func<object, bool> canExecute)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _CanExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter) => _Execute?.Invoke(parameter);
+    }
+}
